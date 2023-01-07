425ca0c68cefaaadbb52b55b895ba9eac2113920
Andi Neck <andi.neck@intesso.com>
2023-01-07 16:42:43 +0100

    update wildcard cert dns retrieval

--- letsencrypt-tunnel.sh
+++ letsencrypt-tunnel.sh
@@ -6,6 +6,14 @@ sudo ln -s /etc/nginx/sites-available/tunnel.conf /etc/nginx/sites-enabled/
 
 nginx -s reload
 
-certbot -d tunnel.intesso.io -d *.tunnel.intesso.io --nginx
+# https://ongkhaiwei.medium.com/generate-lets-encrypt-certificate-with-dns-challenge-and-namecheap-e5999a040708
+# https://www.linkedin.com/pulse/wildcard-certificates-using-lets-encrypt-certbot-pallavi-udhane
+
+# certbot -d tunnel.intesso.io -d *.tunnel.intesso.io --nginx
+
+certbot --preferred-challenges=dns -d tunnel.intesso.io -d *.tunnel.intesso.io --nginx
+
+certbot certonly --manual --preferred-challenges dns -d tunnel.intesso.io -d *.tunnel.intesso.io
+# you have to setup nginx manually
 
 # sudo certbot renew --dry-run
