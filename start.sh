b904a6a00e0ab62b035de6bea8096a7f7fc7029c
Andi Neck <andi.neck@intesso.com>
2023-01-07 21:57:05 +0100

    fix secure flag

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@
 cd localtunnel-server
 
 # server set to run on port 9876
-bin/server --port 9876 --domain tunnel.intesso.io
+bin/server --port 9876 --secure true --domain tunnel.intesso.io 
 
 # example client use to expose local app on port 9000
 # lt --host http://tunnel.intesso.io:9876 --port 9000
