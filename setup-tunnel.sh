5a0f0d1e592a45e01de9e228ccbbd884dd471c3d
Andi Neck <andi.neck@intesso.com>
2023-01-07 14:23:06 +0100

    fix nginx wildcard

--- setup-tunnel.sh
+++ setup-tunnel.sh
@@ -8,5 +8,6 @@ npm install
 # server set to run on port 9876
 # bin/server --port 9876
 
-# example client use to expose local app on port 9000
-# lt --host http://tunnel.intesso.io:9876 --port 9000
+# example client use to expose local app on port 8080
+# lt --host http://intesso.io:9876 --subdomain test --port 8080
+# lt --host https://tunnel.intesso.io --subdomain secure-test --port 8080
